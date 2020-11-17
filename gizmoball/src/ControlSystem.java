473588d814fb13e43ec7abf03424a728faa03fee
101851011281 <10185101281@stu.ecnu.edu.cn>
2020-11-17 14:25:49 +0800

    Update ControlSystem.java

--- gizmoball/src/ControlSystem.java
+++ gizmoball/src/ControlSystem.java
@@ -1,12 +1,19 @@
 import java.util.*;
-
+import javax.swing.*;
+import java.awt.*;
 /**
  * @Author
  * @Date 2020/11/17 9:30
  * @Version 1.0
  */
 public class ControlSystem {
+    private static  void createUserPanel(){
+        UserPanel userPanel = new UserPanel("Gizmobll");
+        userPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        userPanel.setSize(1200, 800);
+        userPanel.setVisible(true);
+    }
     public static void main(String[] args){
-        
+        createUserPanel();
     }
 }
