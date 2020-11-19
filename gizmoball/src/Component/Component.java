2e57212e907c0045d2f8702fd09bdc2624b45e00
Xiang-Li-oss <2300049883@qq.com>
2020-11-19 18:42:17 +0800

    update

--- gizmoball/src/Component/Component.java
+++ gizmoball/src/Component/Component.java
@@ -1,4 +1,36 @@
 package Component;
+/**
+ * @Author LiXiang
+ * @Date 2020/11/19 15:30
+ * @Version 1.0
+ */
+public interface Component {
+    /**
+     * @Author LiXiang
+     * @Date 2020/11/19 15:30
+     * @Version 1.0
+     * 放大控件
+     * @param scaler 放大倍数
+     */
+    public void enlarge(Integer scaler);
 
-public class Component {
+
+    /**
+     * @Author LiXiang
+     * @Date 2020/11/19 15:30
+     * @Version 1.0
+     * 缩小控件
+     * @param scaler 放大倍数
+     */
+    public void shrink(Integer scaler);
+
+    /**
+     * @Author LiXiang
+     * @Date 2020/11/19 15:30
+     * @Version 1.0
+     * 旋转控件
+     * @param angle 放大倍数
+     */
+    public void rotate(Integer angle);
+
 }
--- gizmoball/src/Component/Triangle.java
+++ gizmoball/src/Component/Triangle.java
@@ -1,8 +1,33 @@
 package Component;
 
-public class Triangle {
-    Double origin_x;   //斜边中点坐标
-    Double origin_y;
-    Double length;     //边长
-    Double angle;
+public class Triangle implements Component{
+    Integer origin_x;   //外接正方形左上角坐标
+    Integer origin_y;
+    Integer length;     //边长
+    Integer angle;
+    
+    public Triangle(Integer x,Integer y){
+        this.origin_x = x;
+        this.origin_y = y;
+        length = 20;
+        angle = 0;
+    }
+
+
+    @Override
+    public void enlarge(Integer scaler) {
+        length = length*scaler;
+
+    }
+
+    @Override
+    public void shrink(Integer scaler) {
+        length = length/scaler;
+
+    }
+
+    @Override
+    public void rotate(Integer angle) {
+
+    }
 }
