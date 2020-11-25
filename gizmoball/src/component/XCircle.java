7df6d040718c910ebcf550cb01ab52929b1158a2
Xiang-Li-oss <2300049883@qq.com>
2020-11-25 21:54:15 +0800

    update

--- gizmoball/src/component/XCircle.java
+++ gizmoball/src/component/XCircle.java
@@ -12,6 +12,7 @@ public class XCircle extends XComponent {
 
     public XCircle(Integer x,Integer y,Board board){
         super(x,y,board);
+
     }
 
     /**
@@ -50,15 +51,28 @@ public class XCircle extends XComponent {
         super.judge(ball);
         //控件圆心坐标
         Integer center_x1 = x + length/2;
-        Integer center_y1 = y - length/2;
+        Integer center_y1 = y + length/2;
         //小球圆心坐标
         Integer center_x2 = ball.getX();
         Integer center_y2 = ball.getY();
         //圆心间距离
         double distance = Math.sqrt(Math.pow(center_x1-center_x2,2)+Math.pow(center_y1-center_y2,2));
-        if(distance<=length/2+ball.getRadius())//如果圆心距离小于半径和，则相交
+        if(distance<(length/2+ball.getRadius()))//如果圆心距离小于半径和，则相交
         {
+            System.out.println("圆心坐标：");
+            System.out.print("("+center_x1+","+center_y1+")");
+            System.out.print("("+center_x2+","+center_y2+")");
+            System.out.println("距离："+distance);
+            System.out.println(length/2+ball.getRadius());
+            System.out.println("old:("+ball.getX()+","+ball.getY()+")");
+            System.out.println("before:x:"+ball.getVx()+" y:"+ball.getVy());
+            Double new_x2 = center_x1+(length/2+ ball.getRadius())/distance*(center_x2-center_x1);
+            Double new_y2 = center_y1 + (length/2+ball.getRadius())/distance*(center_y2-center_y1);
+            ball.setX(new_x2.intValue());
+            ball.setY(new_y2.intValue());
+            System.out.println("new:("+ball.getX()+","+ball.getY()+")");
             react(ball);
+            System.out.println("after:x:"+ball.getVx()+" y:"+ball.getVy());
         }
     }
 
@@ -67,7 +81,7 @@ public class XCircle extends XComponent {
         super.react(ball);
         //控件圆心坐标
         double center_x1 = x.doubleValue() + length.doubleValue()/2;
-        double center_y1 = y.doubleValue() - length.doubleValue()/2;
+        double center_y1 = y.doubleValue() + length.doubleValue()/2;
         //小球圆心坐标
         double center_x2 = ball.getX().doubleValue();
         double center_y2 = ball.getY().doubleValue();
@@ -76,6 +90,7 @@ public class XCircle extends XComponent {
         //切线与水平方向夹角的sin和cos
         double sin_a = 1/Math.sqrt(1+k*k);
         double cos_a = k/Math.sqrt(1+k*k);
+        System.out.println("正余弦："+sin_a+" "+cos_a);
         //获取小球当前x方向和y方向分速度
         double vx = ball.getVx();
         double vy = ball.getVy();
@@ -83,13 +98,46 @@ public class XCircle extends XComponent {
         //x方向速度在碰撞面切向和法向做分解
         double vxt = vx*cos_a;
         double vxn = vx*sin_a;
+        System.out.println("x方向速度在碰撞面切向和法向做分解:切向："+vxt+"法向："+vxn);
 
         //切向速度和法向速度再沿xy轴分解
-        double vxtx = vxt*cos_a;
-        double vxty = vxt*sin_a;
+        double vxtx=0;//切向速度在x轴投影
+        double vxty=0;//切向速度在y轴投影
+        double vxnx=0;//法向速度在x轴投影
+        double vxny=0;//法向速度在y轴投影
+        if(center_x2>center_x1&&center_y2<center_y1)//第一象限，右上方
+        {
+            vxtx = vxt * cos_a;//切向速度*cos
+            vxty = vxt * sin_a;//切向速度在*sin
+            vxnx = vxn*sin_a;//法向速度*sin
+            vxny = -vxn*cos_a;//法向速度*cos
+        }
+        else if(center_x2<center_x1&&center_y2<center_y1)//第二象限，左上方
+        {
+            vxtx = vxt * cos_a;//切向速度*cos
+            vxty = -vxt * sin_a;//切向速度在*sin
+            vxnx = vxn*sin_a;//法向速度*sin
+            vxny = vxn*cos_a;//法向速度*cos
+        }
+        else if(center_x2<center_x1&&center_y2>center_y1)//第三象限，左下方
+        {
+            vxtx = vxt * cos_a;//切向速度*cos
+            vxty = vxt * sin_a;//切向速度在*sin
+            vxnx = vxn*sin_a;//法向速度*sin
+            vxny = -vxn*cos_a;//法向速度*cos
+        }
+        else if(center_x2>center_x1&&center_y2>center_y1)//第四象限，右下方
+        {
+            vxtx = vxt * cos_a;//切向速度*cos
+            vxty = -vxt * sin_a;//切向速度在*sin
+            vxnx = vxn*sin_a;//法向速度*sin
+            vxny = vxn*cos_a;//法向速度*cos
+        }
 
-        double vxnx = vxn*sin_a;
-        double vxny = vxn*cos_a;
+        System.out.println("X切向分解，x轴："+vxtx+"y轴："+vxty);
+
+
+        System.out.println("X法向分解，x轴："+vxnx+"y轴："+vxny);
 
         //法向速度反向
         vxnx = -vxnx;
@@ -98,17 +146,38 @@ public class XCircle extends XComponent {
         double Xspeed1 = vxtx + vxnx;
         double Yspeed1 = vxty + vxny;
 
+
+        System.out.println("XSpeed1:"+Xspeed1+" YSpeed1:"+Yspeed1);
         //y方向速度在碰撞面切向和法向做分解
-        double vyt = vy*cos_a;
-        double vyn = vy*sin_a;
+        double vyt = vy*sin_a;
+        double vyn = vy*cos_a;
+        System.out.println("y方向速度在碰撞面切向和法向做分解:切向："+vyt+"法向："+vyn);
 
         //切向速度和法向速度再沿xy轴分解
-        double vytx = vyt*cos_a;
-        double vyty = vyt*sin_a;
+        double vytx;//切向速度在x轴投影
+        double vyty;//切向速度在y轴投影
+        double vynx;//法向速度在x轴投影
+        double vyny;//法向速度在y轴投影
+        if((center_x2>center_x1&&center_y2<center_y1)||(center_x2<center_x1&&center_y2>center_y1))//第一、四象限
+        {
+            vytx = vyt * cos_a;
+            vyty = vyt * sin_a;
+            vynx = -vyn*sin_a;
+            vyny = vyn*cos_a;
+        }
+        else
+        {
+            vytx = -vyt * cos_a;
+            vyty = vyt * sin_a;
+            vynx = vyn*sin_a;
+            vyny = vyn*cos_a;
+        }
 
-        double vynx = vyn*sin_a;
-        double vyny = vyn*cos_a;
+        System.out.println("Y切向分解，x轴："+vytx+"y轴："+vyty);
 
+
+
+        System.out.println("Y法向分解，x轴："+vynx+"y轴："+vyny);
         //法向速度反向
         vynx = -vynx;
         vyny = -vyny;
@@ -116,12 +185,14 @@ public class XCircle extends XComponent {
         double Xspeed2 = vytx + vynx;
         double Yspeed2 = vyty + vyny;
 
+        System.out.println("XSpeed2:"+Xspeed2+" YSpeed2:"+Yspeed2);
 
         Double new_vx = Xspeed1 + Xspeed2;
         Double new_vy = Yspeed1 + Yspeed2;
 
-        ball.setVx(new_vx.intValue());
-        ball.setVy(new_vy.intValue());
+        ball.setVx((int) Math.round(new_vx));
+        ball.setVy((int) Math.round(new_vy));
 
     }
+
 }
