04a8b7a247e1f8b5606ea41585a542232e15a916
101851011281 <10185101281@stu.ecnu.edu.cn>
2020-11-25 09:28:18 +0800

    update

--- gizmoball/src/component/XRectangle.java
+++ gizmoball/src/component/XRectangle.java
@@ -32,6 +32,13 @@ public class XRectangle extends XComponent {
             Image image;
             image = Toolkit.getDefaultToolkit().getImage(picturePath);
             g.drawImage(image, x, y,length,length,board);
+            if(isSelected){
+                Graphics2D g2 = (Graphics2D)g;
+                g2.setColor(Color.RED);
+                //BasicStroke s = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[]{5.0f}, 0.0f);
+                g2.setStroke(new BasicStroke(3));
+                g2.drawRect(x,y,length,length);
+            }
         } catch (Exception e) {
             System.out.println("File Not Found");
         }
