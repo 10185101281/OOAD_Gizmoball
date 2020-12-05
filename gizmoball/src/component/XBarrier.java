94409d22f780e0b4e015e8cc0a8b1a8fc1a0e891
Xiang-Li-oss <2300049883@qq.com>
2020-12-05 21:54:28 +0800

    Update XBarrier.java

--- gizmoball/src/component/XBarrier.java
+++ gizmoball/src/component/XBarrier.java
@@ -16,9 +16,83 @@ public class XBarrier extends XComponent{
     }
 
     @Override
+    public void react(BouncingBall ball) {
+        super.react(ball);
+        //小球圆心的坐标
+        Integer center_x = ball.getX();
+        Integer center_y = ball.getY();
+        //小球半径
+        Integer radius = ball.getRadius();
+        Integer status = is_collision(ball);
+        if(status==1)//左边
+        {
+            System.out.println("边界："+x);
+            System.out.println("before:ball"+ball.getX()+","+ball.getY());
+            System.out.println("before2:ball"+center_x+","+center_y);
+
+            System.out.println(center_x+radius-x);
+            ball.setX(center_x-(center_x+radius-x+1));
+            System.out.println("after:ball"+ball.getX()+","+ball.getY());
+            System.out.println("before:ball"+ball.getVx()+","+ball.getVy());
+            ball.setVx(-ball.getVx());
+            System.out.println("after:ball"+ball.getVx()+","+ball.getVy());
+
+        }
+        else if(status==2)//右边
+        {
+            ball.setX(center_x+(x+length-(center_x-radius)+1));
+            ball.setVx(-ball.getVx());
+        }
+        else if(status==3)//上边
+        {
+            ball.setY(center_y-(center_y+radius-y+1));
+            ball.setVy(-ball.getVy());
+        }
+        else if(status==4)//下边
+        {
+            ball.setY(center_y+(y+length-(center_y-radius)+1));
+            ball.setVy(-ball.getVy());
+            System.out.println(ball.getX()+","+ball.getY());
+        }
+
+    }
+
+    @Override
     public Integer is_collision(BouncingBall ball) {
+
+        //小球圆心的坐标
+        Integer center_x = ball.getX();
+        Integer center_y = ball.getY();
+        //小球半径
+        Integer radius = ball.getRadius();
+        if(center_x>x-radius&&center_x<x&&y<=center_y&&center_y<=y+length)//左边
+        {
+            System.out.println("左边");
+            return 1;
+        }
+        else if(center_x>x+2*length&&center_x<x+2*length+radius&&y<=center_y&&center_y<=y+length)//右边
+        {
+            System.out.println("右边");
+            return 2;
+        }
+
+        else if(center_y<y&&center_y>y-radius&&x-radius<=center_x&&center_x<=x+2*length+radius)//上边
+        {
+            System.out.println("上边");
+            return 3;
+        }
+        else if(center_y>y+length&&center_y<y+length+radius&&x-radius<=center_x&&center_x<=x+2*length+radius)//下边
+        {
+            System.out.println("下边");
+
+            // System.out.println("下边界"+(y+length));
+            // System.out.println("下方"+ball.getX()+","+ball.getY());
+            return 4;
+
+        }
         return 0;
     }
+
 
 
     /**
